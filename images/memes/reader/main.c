13596433e687624a7b0a3bdcf840a7dba6538c62
jovanblazek <36740941+jovanblazek@users.noreply.github.com>
2018-07-03 12:59:32 +0200

    Add files via upload

---
+++ images/memes/reader/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <dirent.h>
+
+#define PRIECINOK "other"
+
+int main(void)
+{
+	struct dirent *de;
+	
+	FILE *f = fopen("vypis.txt","w");
+
+	// opendir() returns a pointer of DIR type. 
+	DIR *dr = opendir("../"PRIECINOK);
+
+	if (dr == NULL) // opendir returns NULL if couldn't open directory
+	{
+		printf("Could not open current directory" );
+		return 0;
+	}
+
+	while ((de = readdir(dr)) != NULL)
+	{
+		printf("<div class=\"box\"><img src=\"images/memes/%s/%s\" alt=\"%s\"></div>\n", PRIECINOK, de->d_name, de->d_name);
+		fprintf(f,"<div class=\"box\"><img src=\"images/memes/%s/%s\" alt=\"%s\"></div>\n", PRIECINOK, de->d_name, de->d_name);
+		/*
+	   	printf("images/memes/%s/%s ", PRIECINOK, de->d_name);
+	    printf("alt=");
+		printf("%s", de->d_name);
+		printf(">\n");*/
+	}
+	closedir(dr); 
+	
+	fclose(f);
+	
+	return 0;
+}
+
