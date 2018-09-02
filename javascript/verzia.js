8d0a310de6ec25f79d2ebb66cec9e68dcce04c0e
jovanblazek <36740941+jovanblazek@users.noreply.github.com>
2018-09-02 19:48:03 +0200

    Add files via upload

--- javascript/verzia.js
+++ javascript/verzia.js
@@ -1,2 +1,2 @@
-document.getElementById("verzia").innerHTML = "4.A Memes v0.6.1 23.08.2018";
-document.getElementById("cas").innerHTML = "Od začiatia projektu uplynulo: ~32hod";
+document.getElementById("verzia").innerHTML = "4.A Memes v1.0 02.09.2018";
+document.getElementById("cas").innerHTML = "Od začiatia projektu uplynulo: ~33hod.";
