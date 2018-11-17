19fcf69d104c3a9792355ad2535494a2a92a6daa
jovanblazek <36740941+jovanblazek@users.noreply.github.com>
2018-11-17 18:57:56 +0100

    Add files via upload

---
+++ javascript/blockView.js
@@ -0,0 +1,30 @@
+// ===== Hide content ====
+
+var blok = document.getElementsByClassName("blok-btn");
+var i;
+
+for (i = 0; i < blok.length; i++) 
+{
+  blok[i].addEventListener("click", function() 
+    {
+        this.classList.toggle("active");
+
+        var rodic = this.parentElement;
+        var panel = rodic.nextElementSibling;
+
+        if (panel.style.maxHeight)
+        {
+          panel.style.maxHeight = null;
+        } 
+        else 
+        {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
+
+// ===== Expand 1st onload ====
+function expandFirst()
+{
+    blok[0].click();
+}
--- javascript/verzia.js
+++ javascript/verzia.js
@@ -1,2 +1,2 @@
-document.getElementById("verzia").innerHTML = "4.A Memes v1.1.2 01.11.2018";
-document.getElementById("cas").innerHTML = "Od začiatia projektu uplynulo: ~39hod.";
+document.getElementById("verzia").innerHTML = "4.A Memes v1.2 17.11.2018";
+document.getElementById("cas").innerHTML = "Od začiatia projektu uplynulo: ~45hod.";
