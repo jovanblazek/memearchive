9015ea79639ceea74372fa1302d729f989216264
jovanblazek <36740941+jovanblazek@users.noreply.github.com>
2019-02-21 12:59:39 +0100

    Add files via upload

--- javascript/blockView.js
+++ javascript/blockView.js
@@ -26,5 +26,7 @@ for (i = 0; i < blok.length; i++)
 // ===== Expand 1st onload ====
 function expandFirst()
 {
-    setTimeout(function(){ blok[0].click() }, 1000)
+    setTimeout(function(){ blok[0].click() }, 600)
+    setTimeout(function(){ blok[0].click() }, 200)
+    setTimeout(function(){ blok[0].click() }, 200)
 }
